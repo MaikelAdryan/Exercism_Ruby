34a18aec0ee7e3cdee484c1b17e03c9b45269cbd
adryanmaikel <adryanmaikel@gmail.com>
2023-08-11 23:01:22 -0300

    completed Resistor Color Duo

--- resistor_color_duo.rb
+++ resistor_color_duo.rb
@@ -2,19 +2,19 @@
 # Resistor Color Duo
 
 module ResistorColorDuo
-  RESISTOR_COLOR = {
-    :black => 0,
-    :brown => 1,
-    :red => 2,
+  BLEND = {
+    :black  => 0,
+    :brown  => 1,
+    :red    => 2,
     :orange => 3,
     :yellow => 4,
-    :green => 5,
-    :blue => 6,
+    :green  => 5,
+    :blue   => 6,
     :violet => 7,
-    :grey => 8,
-    :white => 9,
+    :grey   => 8,
+    :white  => 9
   }
   def self.value colors
-    (RESISTOR_COLOR[colors[0].to_sym].to_s + RESISTOR_COLOR[colors[1].to_sym].to_s).to_i
+    (BLEND[colors[0].to_sym].to_s + BLEND[colors[1].to_sym].to_s).to_i
   end
 end
