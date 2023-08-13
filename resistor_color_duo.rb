27a9a7141dc11344cef19c6ea1806c3223fd2eef
adryanmaikel <adryanmaikel@gmail.com>
2023-08-13 14:25:19 -0300

    updating Resistor Color Duo

--- resistor_color_duo.rb
+++ resistor_color_duo.rb
@@ -3,18 +3,22 @@
 
 module ResistorColorDuo
   COLORS = {
-    :black  => 0,
-    :brown  => 1,
-    :red    => 2,
-    :orange => 3,
-    :yellow => 4,
-    :green  => 5,
-    :blue   => 6,
-    :violet => 7,
-    :grey   => 8,
-    :white  => 9
+    "black" =>  0,
+    "brown" =>  1,
+    "red" =>    2,
+    "orange" => 3,
+    "yellow" => 4,
+    "green" =>  5,
+    "blue" =>   6,
+    "violet" => 7,
+    "grey" =>   8,
+    "white" =>  9
   }
   def self.value colors
-    (COLORS[colors[0].to_sym].to_s + COLORS[colors[1].to_sym].to_s).to_i
+    colors_index = ""
+    colors[..1].each_with_object(COLORS) do |index , colors|
+      colors_index += colors[index].to_s
+    end
+    colors_index.to_i
   end
 end
