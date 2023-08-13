0436e754263e9424adde8a6353aba4607bc9eef8
adryanmaikel <adryanmaikel@gmail.com>
2023-08-13 20:13:37 -0300

    completed Amusement Park

---
+++ amusement-park/attendee.rb
@@ -0,0 +1,22 @@
+class Attendee
+  def initialize(height)
+    @height = height
+    @pass_id = nil
+  end
+
+  def height
+    @height
+  end
+
+  def pass_id
+    @pass_id
+  end
+
+  def issue_pass!(pass_id)
+    @pass_id = pass_id
+  end
+
+  def revoke_pass!
+    @pass_id = nil
+  end
+end
